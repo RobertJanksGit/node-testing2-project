5556499cf10f51657d640fff15b324cd69dfe946
Robert Jank <robert.jank@yahoo.com>
2024-11-21 13:33:13 -0600

    wip

--- api/users/user-model.test.js
+++ api/users/user-model.test.js
@@ -12,3 +12,16 @@ beforeEach(async () => {
 test("environment is testing", () => {
   expect(process.env.NODE_ENV).toBe("testing");
 });
+
+describe("getAll", () => {
+  test("[1] resolves all the users in the table", async () => {
+    const users = await Users.getAll();
+    expect(users).toHaveLength(2);
+  });
+});
+describe("getById", () => {
+  test("[2] resolves user by user is", async () => {
+    const user = await Users.getById(2);
+    expect(user).toBe("Bob");
+  });
+});
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,1 +1,10 @@
 const db = require("../../data/db-config");
+
+function getAll() {
+  return db("users");
+}
+
+function getById(id) {
+  return db("users").where("id", id).first();
+}
+module.exports = { getAll, getById };
